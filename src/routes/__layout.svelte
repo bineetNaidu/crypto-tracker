48c0a699fba3b839afa667fc6962a3044cea15ab
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-20 10:45:10 +0530

    🐛 clean ups!

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import Header from '$lib/Header/index.svelte';
 	import '../app.css';
 </script>
-
-<Header />
 
 <main>
 	<slot />
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 </footer>
 
 <style>
@@ -31,10 +27,6 @@
 		justify-content: center;
 		align-items: center;
 		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
